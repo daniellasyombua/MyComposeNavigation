91a3d5c1fdcb80cc5c658e260d34063b58787847
Daniella Syombua <syombuadaniella@gmail.com>
2025-07-08 21:18:08 +0300

    Added bottom navigation to my home page

--- app/src/main/java/com/daniella/navigation/nav/Navigation.kt
+++ app/src/main/java/com/daniella/navigation/nav/Navigation.kt
@@ -1,17 +1,37 @@
 package com.daniella.navigation.nav
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Create
+import androidx.compose.material.icons.sharp.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.daniella.navigation.screens.HomeScreen
 import com.daniella.navigation.screens.LoginScreen
 import com.daniella.navigation.screens.SignUpScreen
+import androidx.compose.material.icons.sharp.Settings
+import androidx.compose.material.icons.sharp.ShoppingCart
+
 
 sealed class Screen(val route: String){
     object Login:Screen("login")
     object SignUp:Screen("signup")
     object Home:Screen("home")
+}
+
+sealed class HomeTab(val route:String, var title:String, var icon: ImageVector){
+
+    object Home: HomeTab("hometab","Home", Icons.Sharp.Home)
+    object Products: HomeTab("products","Products", Icons.Sharp.ShoppingCart)
+    object Payments: HomeTab("payments","Payments", Icons.Sharp.Create)
+    object Settings: HomeTab("settings","Settings", Icons.Sharp.Settings)
+
+
+
+
+
 }
 
 @Composable
--- app/src/main/java/com/daniella/navigation/screens/Home.kt
+++ app/src/main/java/com/daniella/navigation/screens/Home.kt
@@ -1,25 +1,105 @@
 package com.daniella.navigation.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.daniella.navigation.nav.HomeTab
+import com.daniella.navigation.nav.HomeTab.Home
+import com.daniella.navigation.nav.HomeTab.Payments
+import com.daniella.navigation.nav.HomeTab.Products
+import com.daniella.navigation.nav.HomeTab.Settings
 
 @Composable
 fun HomeScreen(){
+    val navController = rememberNavController()
+    var selectedTab by remember { mutableStateOf(0) }
+
+
+    val tabs = listOf(HomeTab.Home, HomeTab.Products, HomeTab.Payments, HomeTab.Settings)
+
+    Scaffold (bottomBar = {
+        NavigationBar {
+    tabs.forEachIndexed{index,tab ->
+        NavigationBarItem(
+            icon = {
+                Icon(imageVector =  tab.icon,contentDescription = null)
+                   },
+            label = { Text(text = tab.title) },
+            selected = selectedTab == index,
+            onClick = {selectedTab = index })
+    }
+    }
+//        }
+    }
+    )
+    { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            when (selectedTab) {
+                0 -> HomeContent()
+                1 -> ProductsContent()
+                2 -> PaymentsContent()
+                3 -> SettingsContent()
+            }
+        }
+
+
+
+
+
     Column (modifier = Modifier.fillMaxSize().padding(16.dp)){
         Text(text = "HOME" , fontWeight = FontWeight.Bold, fontSize = 20.sp)
     }
 }
+}
 
 
 
+
+@Composable
+fun HomeContent(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Home")
+    }
+}
+
+@Composable
+fun ProductsContent(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Product")
+    }
+}
+
+@Composable
+fun PaymentsContent(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Payment")
+    }
+}
+
+@Composable
+fun SettingsContent(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Settings")
+    }
+}
 
 
 
